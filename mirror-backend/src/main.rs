7b456d20eaac567e0a288191b1d2bba1c2dbeeee
aj-ats <andowens@advancedtech.com>
2024-07-03 15:11:22 -0500

    structured dirs for buidl

---
+++ mirror-backend/src/main.rs
@@ -0,0 +1,29 @@
+//hello rust to learn before I build server 
+// walked thru this page before building my simple calculato
+/// #Process 
+/// client askes for user input - choose a mathmatical operation
+/// client askes user for input - x y values 
+/// client returns sum
+// use std::io;
+// mod module {
+//     pub fn add2(x:i32, y:i32) -> i32 {
+//         x + y
+//     }
+//     pub fn subtract2(x:i32, y:i32) -> i32 {
+//         x - y
+//     }
+//     pub fn multiply2(x:i32, y:i32) -> i32 {
+//         x * y 
+//     }
+//     pub fn divide2(x:i32, y:i32) -> i32 {
+//         x / y 
+//     }
+// }
+
+
+fn main() {
+    println!("hellowrld")
+}
+
+
+
